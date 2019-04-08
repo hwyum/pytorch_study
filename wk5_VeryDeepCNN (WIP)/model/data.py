95f1f07a80500a323f07a108735a3699f3f7fad2
hwyum <gPdnjs-1>
2019-04-08 10:15:00 +0900

    wk5 VDCNN created

---
+++ wk5_VeryDeepCNN (WIP)/model/data.py
@@ -0,0 +1,35 @@
+from torch.utils.data import Dataset
+import torch
+import pandas as pd
+
+
+class MovieDataJaso(Dataset):
+    """
+    jaso level movie dataset class
+    """
+
+    def __init__(self, filepath, tokenizer, padder) -> None:
+        """
+        initializing the class
+        :param filepath: dataset file path
+        :param tokenizer: tokenizer used to tokenize into jaso level
+        :param padder: padder used to pad when the length of the sequence is less than max_len
+        """
+
+        self.data = pd.read_table(filepath)
+        self.tokenizer = tokenizer
+        self.padder = padder
+
+    def __len__(self) -> int:
+        """return dataset length"""
+        return len(self.data)
+
+    def __getitem__(self, idx) -> torch.Tensor:
+        """ return specific index of the data in model input type """
+
+        document = self.data.iloc[idx].document
+        label = self.data.iloc[idx].label
+        document_tokenized = self.padder(self.tokenizer.tokenize_and_transform(document))
+        sample = (torch.tensor(document_tokenized), torch.tensor(label)) # transform into torch tensor
+
+        return sample
---
+++ wk5_VeryDeepCNN (WIP)/model/network.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class VDCNN(nn.Module):
+    """ implementation of VDCNN model architecture """
+    def __init__(self) -> None:
+
+        super(VDCNN, self).__init__()
+
+
+    def forward(self, x:torch.tensor) -> torch.tensor:
+        return
+
+    def __init_weight(self, layer) -> None:
+        return
+
+
