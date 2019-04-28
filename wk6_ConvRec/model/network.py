d842fb7db14508adf584d35896af54d297e5a7d3
Haewon Yum <yum.haewon@gmail.com>
2019-04-28 14:28:54 +0900

    network updated

--- wk6_ConvRec/model/network.py
+++ wk6_ConvRec/model/network.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import ModuleList
 import torch.nn.functional as F
-from model.modules import Embedding, Permute, ConvLayer, RecLayer, Flatten
+from model.modules import Embedding, Permute, ConvLayer, RecLayer, Flatten, Pack_padded_seq, Dropout
 from typing import List
 
 
@@ -22,17 +22,17 @@ class ConvRec(nn.Module):
         self._convLayers = ModuleList([ConvLayer(i, o, k, p) for i, o, k, p in zip(conv_in_channels, conv_out_channels,
                                                                                    conv_kernel_size, conv_pooling_size)])
 
-        self._recLayer = RecLayer(input_size=conv_out_channels[-1], hidden_size=hidden_size)
-
         self._ops = nn.Sequential(Embedding(num_embedding, embedding_dim),  # output_shape : NLC
                                   Permute(),   # output_shape : NCL
                                   *self._convLayers,  # output_shape : NCL
-                                  nn.Dropout(0.5),
-                                  self._recLayer,    # output_shape : NLC
+                                  Dropout(),
+                                  Permute(),  # batch x channel x length -> batch x length x channel
+                                  Pack_padded_seq(),
+                                  RecLayer(conv_out_channels[-1], hidden_size),  # output_shape : NLC
                                   nn.Dropout(0.5),
                                   Flatten(),
                                   nn.Linear(hidden_size * 2, class_num))
 
-    def forward(self, input: torch.tensor) -> torch.tensor:
-        output = self._ops(input)
+    def forward(self, inputs: (torch.Tensor, torch.Tensor)) -> torch.Tensor:
+        output = self._ops(inputs)
         return output
