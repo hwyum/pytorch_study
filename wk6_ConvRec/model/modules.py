82974c5d4bf1c5c5ee78f313bf1fefbe0d0612a3
Haewon Yum <yum.haewon@gmail.com>
2019-04-28 14:27:21 +0900

    lenght traking added

--- wk6_ConvRec/model/modules.py
+++ wk6_ConvRec/model/modules.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence, pad_packed_sequence, pack_sequence, pack_padded_sequence, PackedSequence
 
 class Embedding(nn.Module):
     """ class for Embedding """
@@ -12,9 +13,16 @@ class Embedding(nn.Module):
         """
         super(Embedding, self).__init__()
         self._embedding = nn.Embedding(num_embeddings=num_embedding, embedding_dim=embedding_dim, padding_idx=0)
+        # Embedding output : N x L x C
 
-    def forward(self, input:torch.tensor) -> torch.tensor:
-        return self._embedding(input)
+    def forward(self, inputs: (torch.Tensor, torch.Tensor)) -> (torch.Tensor, torch.Tensor):
+        # print("[Embedding] inputs: {}".format(inputs))
+        input, length = inputs
+        emb_out = self._embedding(input)
+
+        # print("\nInput length: {}".format(length))
+        return emb_out, length
+
 
 class ConvLayer(nn.Module):
     """ class for Convolusional layer with max pooling """
@@ -27,14 +35,42 @@ class ConvLayer(nn.Module):
         :param pooling_size: pooling size for max pooling
         """
         super(ConvLayer, self).__init__()
-        self._conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size)
-        self._maxpool = nn.MaxPool1d(pooling_size)
 
-    def forward(self, input:torch.tensor) -> torch.tensor:
+        self._in_channels = in_channels
+        self._out_channels = out_channels
+        self._kernel_size = kernel_size
+        self._pooling_size = pooling_size
+
+        self._conv = nn.Conv1d(in_channels=self._in_channels, out_channels=self._out_channels, kernel_size=self._kernel_size)
+        self._maxpool = nn.MaxPool1d(self._pooling_size)
+
+    def forward(self, inputs: (torch.Tensor, torch.Tensor)) -> (torch.Tensor, torch.Tensor):
+        input, length = inputs
         output = self._maxpool(self._conv(input))
         output = F.relu(output)
-        # print('output shape for ConvLayer: {}'.format(output.size()))
-        return output
+
+        # length tracking
+        length = length - (self._kernel_size - 1)
+        length = (length - (self._pooling_size - 1) - 1) / (self._pooling_size) + 1
+        # print("\nConv length: {}".format(length))
+        return output, length
+
+
+class Pack_padded_seq(nn.Module):
+    """ class for buiding pack_padded_sequence """
+    def __init__(self) -> None:
+        super(Pack_padded_seq,self).__init__()
+
+    def forward(self, inputs: (torch.Tensor, torch.Tensor)) -> PackedSequence:
+        input, length = inputs  # input: batch x length x hidden
+
+        sorted_idx = torch.argsort(length, descending=True)
+        input = input[sorted_idx]
+        length = length[sorted_idx]
+        # print("input shape:{}, length shape:{}".format(input.size(), length.size()))
+        # print("Rec Input length: {}".format(length))
+        return pack_padded_sequence(input, length, batch_first=True)
+
 
 class RecLayer(nn.Module):
     """ class for Bi-LSTM layer """
@@ -48,28 +84,23 @@ class RecLayer(nn.Module):
         self._bilstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True, bidirectional=True)
         self._dropout = nn.Dropout(0.5)
 
-    def forward(self, input:torch.tensor) -> torch.tensor:
+    def forward(self, input: PackedSequence) -> torch.Tensor:
         """
         input: (batch, seq, feature) when batch_first = True
         """
-        input = input.permute(0, 2, 1)  # batch x channel x length -> batch x length x channel
-        # print('input shape for RecLayer: {}'.format(input.size()))
-
-        output, (hn, cn) = self._bilstm(input)  # output shape: Batch x Seq_len x (Hidden_dim * 2)
+        packed_output, (hn, cn) = self._bilstm(input)  # output shape: Batch x Seq_len x (Hidden_dim * 2)
         # print('output shape for Bi-LSTM: {}'.format(output.size()))
 
         hn_cat = torch.cat([hn[0], hn[1]], dim=1)
         return hn_cat  # output shape: Batch x (Hidden_dim * 2)
 
 
-
 class Flatten(nn.Module):
     """ flattening conv output to feed into FC layers"""
-
     def __init__(self):
         super(Flatten, self).__init__()
 
-    def forward(self, input: torch.tensor) -> torch.tensor:
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         return torch.flatten(input, start_dim=1)
 
 
@@ -78,17 +109,19 @@ class Permute(nn.Module):
     def __init__(self):
         super(Permute, self).__init__()
 
-    def forward(self, input: torch.tensor) -> torch.tensor:
-        return input.permute(0, 2, 1)
+    def forward(self, inputs: (torch.Tensor, torch.Tensor)) -> (torch.Tensor, torch.Tensor):
+        input, length = inputs
+        return input.permute(0, 2, 1), length
 
-### Test
-# import numpy as np
-# test = torch.tensor(np.array([1,2,3,4,5]), dtype=torch.float32)
-# test = test.view(1,5,1)
-#
-# padded_indices_wbf = padded_indices_wbf.view(5,13,1)
-#
-# bilstm = nn.LSTM(input_size=1, hidden_size=10, batch_first=True, bidirectional=True)
-# hn, cn = bilstm(padded_indices_wbf)[1]
-# hn[0].size()
-# torch.cat([hn[0],hn[1]], dim=1).size()
+
+class Dropout(nn.Module):
+    """ dropout with keeping length """
+    def __init__(self, prob=0.5):
+        super(Dropout, self).__init__()
+        self._dropout = nn.Dropout(prob)
+
+    def forward(self, inputs: (torch.Tensor, torch.Tensor)) -> (torch.Tensor, torch.Tensor):
+        input, length = inputs
+        return self._dropout(input), length
+
+
