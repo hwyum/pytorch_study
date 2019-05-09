646167e0e1e828a0c23063cd0f4dcaa6d24e5e92
hwyum <gPdnjs-1>
2019-05-09 13:03:47 +0900

    modules updated (wip)

--- wk7_SAN/model/modules.py
+++ wk7_SAN/model/modules.py
@@ -45,28 +45,44 @@ class BiLSTM(nn.Module):
             outputs1, _ = self._bilstm(q1)
             outputs2, _ = self._bilstm(q2)
 
-            hn_cat1 = torch.cat(outputs1[:, :, :self._hidden_size], outputs1[:, :, self._hidden_size:], dim=2)
-            hn_cat2 = torch.cat(outputs2[:, :, :self._hidden_size], outputs2[:, :, self._hidden_size:], dim=2)
+            hn_cat1 = torch.cat((outputs1[:, :, :self._hidden_size], outputs1[:, :, self._hidden_size:]), dim=2)
+            hn_cat2 = torch.cat((outputs2[:, :, :self._hidden_size], outputs2[:, :, self._hidden_size:]), dim=2)
             return hn_cat1, hn_cat2
 
         else:
             outputs, _ = self._inputs     # outputs : batch, seq_len, num_directions * hidden_size)
-            hn_cat = torch.cat(outputs[:,:,:self._hidden_size], outputs[:,:,self._hidden_size:], dim=2)
+            hn_cat = torch.cat((outputs[:,:,:self._hidden_size], outputs[:,:,self._hidden_size:]), dim=2)
             return hn_cat  # output shape: Batch x seq_len x (Hidden_dim * 2)
 
 class SelfAttention(nn.Module):
     """ class for self attention """
     def __init__(self, in_features, hidden_units, hops, is_pair:bool=True) -> None:
+        super(SelfAttention, self).__init__()
         self._is_pair = is_pair
         self._linear_1 = nn.Linear(in_features=in_features, out_features=hidden_units)
         self._linear_2 = nn.Linear(in_features=hidden_units, out_features=hops)
 
     def forward(self, inputs: Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
         if self._is_pair:
-            q1, q2 = inputs  # batch x 2u x n
-            q1_weights = F.softmax(self._linear_2(F.tanh(self._linear_1(q1))), dim=-1)  # batch x r x n (seq_len)
-            q2_weights = F.softmax(self._linear_2(F.tanh(self._linear_1(q2))), dim=-1)
-            q1_representations = torch.matmul()
+            q1_hn, q2_hn = inputs  # batch x 2u x n
+            q1_weights = F.softmax(self._linear_2(F.tanh(self._linear_1(q1_hn))), dim=-1)  # batch x r x n (seq_len)
+            q2_weights = F.softmax(self._linear_2(F.tanh(self._linear_1(q2_hn))), dim=-1)
+            q1_representations = torch.matmul(q1_weights, q1_hn.permute(0,2,1))     # batch x r x 2u
+            q2_representations = torch.matmul(q2_weights, q2_hn.permute(0,2,1))
+
+            return q1_representations, q2_representations
 
         else:
-            weights =  F.softmax(self._linear_2(F.tanh(self._linear_1(inputs))), dim=-1)
+            weights = F.softmax(self._linear_2(F.tanh(self._linear_1(inputs))), dim=-1)
+            representations = torch.matmul(weights, inputs.permute(0,2,1))
+
+            return representations
+
+
+class Permute(nn.Module):
+    """ puermutation """
+    def __init__(self) -> None:
+        super(Permute, self).__init__()
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return input.permute(0, 2, 1), length
--- wk7_SAN/model/network.py
+++ wk7_SAN/model/network.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model.modules import Embedding, BiLSTM
+from model.modules import Embedding, BiLSTM, SelfAttention
 
 class SAN(nn.Module):
     def __init__(self, num_embedding, embedding_dim, hidden_dim) -> None:
@@ -10,6 +10,7 @@ class SAN(nn.Module):
 
         self._ops = nn.Sequential(Embedding(num_embedding, embedding_dim),
                                   BiLSTM(embedding_dim, hidden_dim),
+                                  SelfAttention(2*hidden_dim, 150, 30)
                                   )
 
 
