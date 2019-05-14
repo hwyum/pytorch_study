fc57033921d59a237de5c7811533bad1bb2412d6
Haewon Yum <yum.haewon@gmail.com>
2019-05-14 15:16:38 +0900

    training code updated

--- wk7_SAN/training.py
+++ wk7_SAN/training.py
@@ -22,7 +22,7 @@ import pickle
 # cfgpath = 'config.json'
 
 
-def loss_batch(model, attn_hops, loss_func, q1, q2, y, opt):
+def loss_batch(model, attn_hops, loss_func, q1, q2, yb, opt):
     inputs = (q1, q2)
     outputs = None
     attn_mtx_1 = None
@@ -38,13 +38,13 @@ def loss_batch(model, attn_hops, loss_func, q1, q2, y, opt):
             attn_mtx_1 = outputs[1][0]
             attn_mtx_2 = outputs[1][1]
 
-    loss = loss_func(outputs, y) + penalty(attn_mtx_1, attn_hops, 0.3) + penalty(attn_mtx_2, attn_hops, 0.3)
+    loss = loss_func(outputs, yb) + penalty(attn_mtx_1, attn_hops, 0.3) + penalty(attn_mtx_2, attn_hops, 0.3)
     loss.backward()  ## backprop
     # torch.nn.utils.clip_grad_norm_(model.parameters(), 5, norm_type=2)  # gradient clipping
     opt.step()  ## weight update
     opt.zero_grad()  ## gradient initialize
 
-    return loss.item()
+    return loss.item(), outputs
 
 
 def evaluate(model, loss_func, dataloader, dev):
@@ -54,25 +54,33 @@ def evaluate(model, loss_func, dataloader, dev):
     correct = 0
     num_yb = 0
     for step, mb in enumerate(tqdm(dataloader, desc = 'Validation')):
-        q1, q2, y = map(lambda x: x.to(dev), mb)
-        output = model((q1, q2))
-        loss = loss_func(output, y)
+        q1, q2, yb = map(lambda x: x.to(dev), mb)
+        outputs = model((q1, q2))
+        loss = loss_func(outputs, yb)
         avg_loss += loss.item()
 
         # accuracy calculation
-        _, predicted = torch.max(output, 1)
-        # print('predicted size : ', predicted.size())
-        correct += torch.sum((y == predicted)).item()
-        num_yb += len(y)
+        _correct, _num_yb = accuracy_batch(outputs, yb)
+        correct += _correct
+        num_yb += _num_yb
     else:
         avg_loss /= (step+1)
         accuracy = correct / num_yb
     return avg_loss, accuracy
 
 
-def penalty(attn_mtx: torch.Tensor, hops: int, coefficient):
-    aat = torch.bmm(attn_mtx, attn_mtx.permute(0,2,1))
-    identity = torch.eye(hops)
+def accuracy_batch(outputs, yb):
+    # accuracy calculation
+    _, predicted = torch.max(outputs, 1)
+    correct = torch.sum((yb == predicted)).item()
+    num_yb = len(yb)
+
+    return correct, num_yb
+
+
+def penalty(attn_mtx: torch.Tensor, hops: int, coefficient, dev):
+    aat = torch.bmm(attn_mtx, attn_mtx.permute(0,2,1)).to(dev)
+    identity = torch.eye(hops).to(dev)
     penalty_term = torch.norm((aat - identity), dim=(1,2)) ** 2
     penalty_term = coefficient * penalty_term.mean()
 
@@ -162,11 +170,18 @@ def train(cfgpath):
         model.train()
 #         scheduler.step()
         avg_tr_loss = 0
+        tr_correct = 0
+        tr_num_yb = 0
 
         for step, mb in enumerate(tqdm(tr_dl, desc='Training')):
-            q1, q2, y = map(lambda x: x.to(dev), mb)
-            loss = loss_batch(model, attn_hops, loss_func, q1, q2, y, opt)
+            q1, q2, yb = map(lambda x: x.to(dev), mb)
+            loss, outputs = loss_batch(model, attn_hops, loss_func, q1, q2, yb, opt)
             avg_tr_loss += loss
+
+            # training accuracy
+            _correct, _num_yb = accuracy_batch(outputs, yb)
+            tr_correct += _correct
+            tr_num_yb += _num_yb
 
             if epoch > 0 and (epoch * len(tr_dl) + step) % 500 == 0:
                 val_loss, _ = evaluate(model, loss_func, val_dl, dev)
@@ -175,12 +190,13 @@ def train(cfgpath):
                 model.train()
         else:
             avg_tr_loss /= (step+1)
+            tr_correct /= tr_num_yb
 
         model.eval()
         avg_val_loss, accuracy = evaluate(model, loss_func, val_dl, dev)
 
-        print('Epoch: {}, training loss: {:.3f}, validation loss: {:.3f}, validation accuracy: {:.3f}'
-              .format(epoch, avg_tr_loss, avg_val_loss, accuracy))
+        print('Epoch: {}, training loss: {:.3f}, validation loss: {:.3f}, training accuracy: {:.3f}, validation accuracy: {:.3f}'
+              .format(epoch, avg_tr_loss, avg_val_loss, tr_correct, accuracy))
 
     ckpt = {'epoch': epochs,
             'model_state_dict': model.state_dict(),
