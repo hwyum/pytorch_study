abfaa0b1a5af4d1bac3f2064d4591fa6c5519b28
Haewon Yum <yum.haewon@gmail.com>
2019-09-05 21:27:19 +0900

    model updated

--- wk9_NMT/model/modules.py
+++ wk9_NMT/model/modules.py
@@ -46,15 +46,15 @@ class Encoder(nn.Module):
     """ encoder class """
     def __init__(self, vocab_ko:nlp.Vocab, embedding_dim:int, hidden_dim:int, padding_idx=1):
         super(Encoder,self).__init__()
-        pad_idx = padding_idx
+        self.pad_idx = padding_idx
 
-        self._embedding = nn.Embedding(len(vocab_ko.token_to_idx), embedding_dim, padding_idx=pad_idx) # NLC
+        self._embedding = nn.Embedding(len(vocab_ko.token_to_idx), embedding_dim, padding_idx=self.pad_idx) # NLC
         self._lstm = StackingLSTM(embedding_dim, hidden_dim)
         # self._dev = dev
 
     def forward(self, input_ko):
         embedded = self._embedding(input_ko)
-        lstm_out, last_hidden = self._lstm(embedded)
+        lstm_out, last_hidden = self._lstm(embedded) # last_hidden: (num_layers * num_directions, batch, hidden_size)
         return lstm_out, last_hidden
 
     def init_hidden(self):
@@ -64,27 +64,43 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     """ Decoder class """
     def __init__(self, vocab_tgt:nlp.Vocab, embedding_dim:int, hidden_dim:int):
+        """ Initialization of Decoder """
         super(Decoder,self).__init__()
         self._embedding = nn.Embedding(len(vocab_tgt.token_to_idx), embedding_dim, padding_idx=1)
         self._lstm = StackingLSTM(embedding_dim, hidden_dim)
+        self._wc = nn.Linear(hidden_dim * 2, hidden_dim)
         self._linear = nn.Linear(hidden_dim, len(vocab_tgt.token_to_idx))
         # self._softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, input, hidden_state):
+    def forward(self, inputs, hidden_state, context_vector=None):
         """
+        Args:
+            inputs: (batch,)
+            hidden_state: decoder hidden state in current time step
+            context_vector:
 
-        :param input: (batch,)
-        :param hidden_state: input hidden state
-        :return:
+        Returns:
+            output: decoder output for the current time step
+            next_decoder_hidden: decoder hidden state for next time step
         """
-        embedded = self._embedding(input) # (batch, 1, hidden)
-        lstm_out, next_hidden = self._lstm(embedded, hidden_state)
-        output = self._linear(lstm_out)
+        embedded = self._embedding(inputs) # (batch, 1, hidden)
+        lstm_out, next_decoder_hidden = self._lstm(embedded, hidden_state)
+        next_hidden, next_cell_state = next_decoder_hidden
 
-        # lstm_out: (batch, seq_len, hidden_size)
+        if context_vector is not None:
+            context_hidden_concat = torch.cat((context_vector, next_hidden[-1].unsqueeze(1)), dim=2)
+            attentional_hidden_state = torch.tanh(self._wc(context_hidden_concat))
+            output = self._linear(attentional_hidden_state)
+            output = F.softmax(output, dim=2)
+
+        else:
+            output = self._linear(lstm_out)
+            output = F.softmax(output, dim=2)
+
+        # lstm_out: (batch, 1, hidden_size)
         # hidden: batch, num_layers * num_directions, hidden_size
         #
-        return output, next_hidden
+        return output, next_decoder_hidden
 
 
 class Attention(nn.Module):
@@ -92,8 +108,8 @@ class Attention(nn.Module):
     will use general score function
     * reference: https://machinetalk.org/2019/03/29/neural-machine-translation-with-attention-mechanism/
     """
-    def __init__(self, hidden_dim:int, max_length:int):
-
+    def __init__(self, hidden_dim:int):
+        super(Attention, self).__init__()
         self._wa = nn.Linear(hidden_dim, hidden_dim)
         self._attn = nn.Linear(hidden_dim * 2, hidden_dim)
 
@@ -114,3 +130,87 @@ class Attention(nn.Module):
         context = torch.bmm(alignment, encoder_outputs)
         return context, alignment
 
+
+# Luong attention layer
+# https://pytorch.org/tutorials/beginner/chatbot_tutorial.html#decoder
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        if self.method not in ['dot', 'general', 'concat']:
+            raise ValueError(self.method, "is not an appropriate attention method.")
+        self.hidden_size = hidden_size
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def dot_score(self, hidden, encoder_output):
+        return torch.sum(hidden * encoder_output, dim=2)
+
+    def general_score(self, hidden, encoder_output):
+        energy = self.attn(encoder_output)
+        return torch.sum(hidden * energy, dim=2)
+
+    def concat_score(self, hidden, encoder_output):
+        energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output), 2)).tanh()
+        return torch.sum(self.v * energy, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        # Calculate the attention weights (energies) based on the given method
+        if self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+        elif self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+
+        # Transpose max_length and batch_size dimensions
+        attn_energies = attn_energies.t()
+
+        # Return the softmax normalized probability scores (with added dimension)
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+# https://pytorch.org/tutorials/beginner/chatbot_tutorial.html#decoder
+class LuongAttnDecoderRNN(nn.Module):
+    def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1):
+        super(LuongAttnDecoderRNN, self).__init__()
+
+        # Keep for reference
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # Define layers
+        self.embedding = embedding
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers, dropout=(0 if n_layers == 1 else dropout))
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+        self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        # Note: we run this one step (word) at a time
+        # Get embedding of current input word
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded)
+        # Forward through unidirectional GRU
+        rnn_output, hidden = self.gru(embedded, last_hidden)
+        # Calculate attention weights from the current GRU output
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+        # Concatenate weighted context vector and GRU output using Luong eq. 5
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = torch.tanh(self.concat(concat_input))
+        # Predict next word using Luong eq. 6
+        output = self.out(concat_output)
+        output = F.softmax(output, dim=1)
+        # Return output and final hidden state
+        return output, hidden
--- wk9_NMT/model/network.py
+++ wk9_NMT/model/network.py
@@ -1,53 +1,95 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import gluonnlp as nlp
 from model.modules import Encoder, Decoder, Attention
-
+import random
 
 class Seq2Seq(nn.Module):
     """ model class for seq2seq with attention """
-    def __init__(self, vocab_src:nlp.Vocab, vocab_tgt:nlp.Vocab, embedding_dim:int, hidden_dim:int, num_layers:int=1,
-                 bos_idx=2, eos_idx=3, use_teacher_forcing:bool=True, teacher_forcing_ratio=0.5, use_attention=True):
+    def __init__(self, vocab_src:nlp.Vocab, vocab_tgt:nlp.Vocab, embedding_dim:int, hidden_dim:int, dev,
+                 num_layers:int=1, bos_idx=2, eos_idx=3, use_teacher_forcing:bool=True,
+                 teacher_forcing_ratio=0.5, use_attention=True):
         """ initialization of the class """
+        super(Seq2Seq, self).__init__()
 
-        self._dev = torch.cuda.current_device()
         self._encoder = Encoder(vocab_src, embedding_dim, hidden_dim)
         self._decoder = Decoder(vocab_tgt, embedding_dim, hidden_dim)
-        self._attn = Attention(hidden_dim)
+
+        self._dev = dev
+        self._hidden_dim = hidden_dim
+        self._bos_idx = bos_idx
+        self._eos_idx = eos_idx
+        self._pad_idx = self._encoder.pad_idx
+        self._mask = None
+
+        # global attention related
+        self._use_attention = use_attention
+        self._attn = Attention(self._hidden_dim) if self._use_attention else None
+
+        # teacher forcing related
         self._use_teacher_forcing = use_teacher_forcing
         self._teacher_forcing_ratio = teacher_forcing_ratio
-        self._bos_idx = bos_idx
-        self._use_attention = use_attention
 
-
-    def forward(self, *inputs):
+    def forward(self, inputs):
         src, tgt_in, tgt_out = inputs
         batch_size = src.size()[0]
         max_len = tgt_in.size()[1]
+        mask = (tgt_out != self._pad_idx)
 
-        encoder_out, encoder_hidden = self._encoder(src) # encoder_out : (batch, max_len, hidden_dim * 2) (BiLSTM)
+        encoder_output, encoder_hidden = self._encoder(src) # encoder_out : (batch, max_len, hidden_dim * 2) (BiLSTM)
         decoder_input = torch.full((batch_size, 1), self._bos_idx).long().to(self._dev) # float32 -> int64
-        decoder_hidden = encoder_hidden
+        decoder_hidden = encoder_hidden # initialize decoder's hidden state with encoder's last hidden state
 
-        if self._use_attention:
-            # todo : implementation
-            context_vector = self._attn()
-            pass
-        else:
-            context_vector = encoder_hidden
+        loss = 0
+        nTotals = 0
+        for di in range(max_len):
+            if self._use_attention:
+                decoder_hidden_top = decoder_hidden[0][0].unsqueeze(1)  # top layer's hidden state (batch_size, 1, hidden)
+                context_vector = self._attn(decoder_input, decoder_hidden_top, encoder_output)[0]  # (batch, 1, hidden)
+                decoder_output, next_decoder_hidden = self._decoder(decoder_input, decoder_hidden, context_vector)
+            else:
+                decoder_output, next_decoder_hidden = self._decoder(decoder_input, decoder_hidden)
 
-        if self._use_teacher_forcing: # Teacher forcing: Feed the target as the next input --> 트레이닝 코드로.
-            for di in range(max_len):
-                decoder_output, next_hidden = self._decoder(decoder_input, decoder_hidden)
-                # if decoder_input[:,0]
+            decoded_label = decoder_output.topk(1)[1]
 
+            # calculate and accumulate loss
+            mask_loss, nTotal = self.maskNLLLoss(decoder_output, tgt_out[:,di], mask[:,di], self._dev)
+            loss += mask_loss
+            nTotals += nTotal
 
+            # Teacher forcing: Feed the target as the next input --> 트레이닝 코드로.
+            if self._use_teacher_forcing:
+                if random.random() < self._teacher_forcing_ratio:
+                    decoder_input = tgt_out[:,di].unsqueeze(-1)
+                else:
+                    decoder_input = decoded_label.squeeze(2)
+            else:
+                decoder_input = decoded_label.squeeze(2)
+            decoder_hidden = next_decoder_hidden
+
+        return loss, nTotals
+
+    def maskNLLLoss(self, decoder_output, target, mask, dev):
+        """
+        Calculate average Negative Log Likelihood Loss for mini batch in one time step
+        Args:
+            decoder_output: (batch, 1, tgt_vocab_size)
+            target: (batch, )
+            mask: (batch, )
+            dev: current device (cpu or gpu)
+        return:
+            loss:
+
+        """
+        mask = mask.unsqueeze(-1)
+        nTotal = mask.sum()
+        crossEntropy = -torch.log(torch.gather(decoder_output.squeeze(1), 1, target.unsqueeze(-1)).squeeze(1))
+        loss = crossEntropy.masked_select(mask).mean()
+        loss = loss.to(dev)
+        return loss, nTotal.item()
 
     def to(self, dev):
         super(Seq2Seq, self).to(dev)
         self._dev = dev
-
         return
-
-
-#torch.cat([*encoder_hidden[0]], dim=1).unsqueeze(1)
